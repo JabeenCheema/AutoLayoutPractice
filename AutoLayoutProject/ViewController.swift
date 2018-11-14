a773c75106564c3099ca64134fb9731044300e33
Jabeen Cheema <jabeencheema@pursuit.org>
2018-11-14 14:25:20 -0500

    started project

---
+++ AutoLayoutProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutProject
+//
+//  Created by Jabeen's MacBook on 11/14/18.
+//  Copyright © 2018 Jabeen's MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
